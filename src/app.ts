0b617ff5ef601b90ed8fcffb33ad67e7dcb61e64
Luis Henrique Ries <prof.ries@gmail.com>
2021-12-01 19:24:48 -0300

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import * as express from "express";
 import "./database";
+//var cors = require('cors')
+import * as cors from "cors";
+
 import rotaProduto from './rotas/produto';
 import rotaCategoria from './rotas/categoria';
 
     const app = express();
     const PORTA = 3000;
+
+    app.use(cors());
 
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
